09e33a9c9a42e1039ca9d50b6e1b95dfa4ea1183
Rahul K.A <karahul209@gmail.com>
2024-11-26 21:40:45 -0800

    Feature: Add iiwa7_gripper option to launch file

--- lbr_bringup/lbr_bringup/description.py
+++ lbr_bringup/lbr_bringup/description.py
@@ -58,7 +58,7 @@ class LBRDescriptionMixin:
             name="model",
             default_value=default_value,
             description="The LBR model in use.",
-            choices=["iiwa7", "iiwa14", "med7", "med14"],
+            choices=["iiwa7", "iiwa7_gripper", "iiwa14", "med7", "med14"],
         )
 
     @staticmethod
